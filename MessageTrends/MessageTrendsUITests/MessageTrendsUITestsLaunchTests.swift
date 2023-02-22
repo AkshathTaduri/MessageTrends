9e3bf6458583501557648719b3ee5e8ead625146
AkshathTaduri <akshathtaduri@gmail.com>
2023-02-22 02:33:36 -0600

    initial commit

---
+++ MessageTrends/MessageTrendsUITests/MessageTrendsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MessageTrendsUITestsLaunchTests.swift
+//  MessageTrendsUITests
+//
+//  Created by Akshath Taduri on 2/22/23.
+//
+
+import XCTest
+
+final class MessageTrendsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
